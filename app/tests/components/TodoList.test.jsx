7b462cd0a139a5f1edc040a8db406804c3b03056
Naresh Pallothu <nareshpallothu@gmail.com>
2017-03-23 22:35:01 -0400

    Add test suit for TodoList component

---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require('jquery');
+
+var TodoList = require('TodoList');
+var Todo = require('Todo');
+
+describe('TodoList', () => {
+  it('should exists', () => {
+    expect(TodoList).toExist();
+  });
+
+  it('should render one Todo component for each todo item', () => {
+    var todos = [{
+      id: 1,
+      text: 'Drink water'
+    },{
+      id: 2,
+      text: 'Take pills'
+    }];
+
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);// to find all of the nested components
+    expect(todosComponents.length).toBe(todos.length);
+
+  });
+
+});
