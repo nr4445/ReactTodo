58fe4f977988a7580ef0267a462a7958474a39ae
Naresh Pallothu <nareshpallothu@gmail.com>
2017-03-24 21:22:50 -0400

    Search by text and sorting the todos by non completed

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -23,5 +23,36 @@ module.exports = {
     // } esle {
     //   return [];
     // }
+  },
+
+  filterTodos: function(todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+
+    //filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    //filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      //if you return true item is going to be stay in the array , false make it removed
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+
+    //sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed){
+        return -1;// a should come before b
+      } else if(a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+
+    });
+
+    return filteredTodos;
   }
 };
--- public/bundle.js
+++ public/bundle.js
@@ -25545,14 +25545,18 @@
 	    });
 	  },
 	  render: function render() {
-	    var todos = this.state.todos;
+	    var _state = this.state,
+	        todos = _state.todos,
+	        showCompleted = _state.showCompleted,
+	        searchText = _state.searchText;
 
+	    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
 
 	    return React.createElement(
 	      'div',
 	      null,
 	      React.createElement(TodoSearch, { onSearch: this.handleSearch }),
-	      React.createElement(TodoList, { todos: todos, onToggle: this.handleToggle }),
+	      React.createElement(TodoList, { todos: filteredTodos, onToggle: this.handleToggle }),
 	      React.createElement(AddTodo, { onAddTodo: this.handleAddTodo })
 	    );
 	  }
@@ -34107,6 +34111,35 @@
 	    // } esle {
 	    //   return [];
 	    // }
+	  },
+
+	  filterTodos: function filterTodos(todos, showCompleted, searchText) {
+	    var filteredTodos = todos;
+
+	    //filter by showCompleted
+	    filteredTodos = filteredTodos.filter(function (todo) {
+	      return !todo.completed || showCompleted;
+	    });
+
+	    //filter by searchText
+	    filteredTodos = filteredTodos.filter(function (todo) {
+	      var text = todo.text.toLowerCase();
+	      //if you return true item is going to be stay in the array , false make it removed
+	      return searchText.length === 0 || text.indexOf(searchText) > -1;
+	    });
+
+	    //sort todos with non-completed first
+	    filteredTodos.sort(function (a, b) {
+	      if (!a.completed && b.completed) {
+	        return -1; // a should come before b
+	      } else if (a.completed && !b.completed) {
+	        return 1;
+	      } else {
+	        return 0;
+	      }
+	    });
+
+	    return filteredTodos;
 	  }
 		};
 
