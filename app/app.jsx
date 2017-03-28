851e7308d6cc830f310148f4e6ea5e56137b966c
Naresh Pallothu <nareshpallothu@gmail.com>
2017-03-28 15:55:19 -0400

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,17 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Clean the yard'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
+
 
 //Load foundation library
 //require('style!css!foundation-sites/dist/foundation.min.css')//to style these html we need embed the chain with style loader
